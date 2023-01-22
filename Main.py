ea22cb47913bc803780cb775bbc38c5be59913a5
cdavi <cdavi13@gmail.com>
2023-01-22 18:35:10 -0300

    v1.3

--- Main.py
+++ Main.py
@@ -1,12 +1,21 @@
 import classes
 from classes.Jogador import Jogador
 from classes.npcs import Npc
+def objTest(obj1,obj2):
+    print("")
+    print("")
+    print(obj1)
+    print("")
+    print(obj2)
+    print("")
+    pass
 
 pc = Jogador("Davi" , 100, 15,1)
 
 npc = Npc("Npc", 30, 30,True,2)
 
 
-print(npc)
-print(pc)
+objTest(pc,npc)
+
+
 # print("hello")
